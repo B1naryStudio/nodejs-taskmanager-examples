61f4b9c68f312ef744dcf122bab07a89721a3aa5
Mykyta Semenistyi <nikeiwe@gmail.com>
2015-09-23 23:40:58 +0300

    task: statuses to enum on frontend

--- frontend/javascripts/task/Task.js
+++ frontend/javascripts/task/Task.js
@@ -6,7 +6,17 @@ define(['backbone'], function(Backbone){
 			name: undefined,
 			description: undefined,
 			isArchived: false,
-			status: undefined
+			status: undefined,
+			tasks: [{
+				value: 'to-do',
+				text: 'To Do'
+			}, {
+				value: 'in-progress',
+				text: 'In Progress'
+			}, {
+				value: 'done',
+				text: 'Done'
+			}]
 		},
 		urlRoot: '/api/task'
 	});
